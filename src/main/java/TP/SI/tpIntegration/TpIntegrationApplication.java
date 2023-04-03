4a4c015a69145852c3ed78891a19a306634a1637
MonkeyLuffy9745 <bilazajeff9@gmail.com>
2023-04-03 17:34:26 +0000

    first commit

---
+++ src/main/java/TP/SI/tpIntegration/TpIntegrationApplication.java
@@ -0,0 +1,13 @@
+package TP.SI.tpIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TP/SI/tpIntegration/TpIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package TP.SI.tpIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
